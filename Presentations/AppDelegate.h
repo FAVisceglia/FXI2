ade91782f7020f1aa2125ab1789290e4fb994116
Visceglia Anthony <avisceglia@fxi12585525.home>
2014-12-02 10:43:04 -0500

    Initial commit from local git

---
+++ Presentations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Presentations
+//
+//  Created by Visceglia Anthony on 12/2/14.
+//  Copyright (c) 2014 FXI, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Presentations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Presentations
+//
+//  Created by Visceglia Anthony on 12/2/14.
+//  Copyright (c) 2014 FXI, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
